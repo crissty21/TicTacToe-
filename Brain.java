c469a199a367d78488e28273fb730b7e3e45480a
CristianNicolae <cristiannicolae.cindea@ulbsibiu.ro>
2022-06-02 23:00:20 +0300

    updates

--- Brain.java
+++ Brain.java
@@ -8,51 +8,91 @@ class Element
 {
     int x, y;
     Gride BoxInWorld; //referinta la obiectul prorpiu zis din lume
-    List<List<Element>> Lines= new ArrayList<>(); //lista cu linile din care face parte obiectul
+    List<List<Element>> Lines; //lista cu linile din care face parte obiectul
     Type Val;
     public Element()
     {
         x = y = 0;
         Val = Val.notOpened;
         BoxInWorld = new Gride();
-        
+        Lines = new ArrayList<>();
         for(int i=0;i<4;i++)
             Lines.add(new ArrayList<Element>());
     }
+
     public Element(int CoordX, int CoordY)
     {
         this();
         x = CoordX;
         y = CoordY;
     }
+
     public Element(int CoordX, int CoordY, Gride RefToObject)
     {
         this(CoordX, CoordY);
         BoxInWorld = RefToObject;
     }
+
     public void setStatus(Type newStatus)
     {
         Val = newStatus;
     }
+
     public Type getStatus()
     {
         return Val;
     }
+
     public int getLineLenght(int index)
     {
         return Lines.get(index).size();
     }
+
     public int getCoordX()
     {
         return x;
     }
+
     public int getCoordY()
     {
         return y;
     }
+
     public void addOnLine(int index, Element vecin)
     {
         Lines.get(index).add(vecin);
+    }
+    public void addLineOnLine(int index, List<Element> newLine)
+    {
+        Lines.get(index).addAll(newLine);
+    }
+    public List<Element> getLine(int index)
+    {
+        return Lines.get(index);
+    }
+    public boolean containsOnLine(int line, Element second)
+    {
+        return Lines.get(line).contains(second);
+    }
+
+    public void sincLines()
+    {
+        for(int line=0;line<=3;line++)
+        {
+            int sizeOfLine = Lines.get(line).size();
+            //luam fiecare vecin 
+            for(int i = 1;i<sizeOfLine;i++)
+            {
+                //adaugam in lista vecinului, toate elemente ce lipsesc
+                for(int j = 0; j<sizeOfLine;j++)
+                {
+                    if(Lines.get(line).get(i).containsOnLine(line, Lines.get(line).get(j)) == false)
+                    {
+                        Lines.get(line).get(i).addOnLine(line, Lines.get(line).get(j));
+                    }
+                }
+            }
+        }
     }
 };
 //clasa folosita pentru override la metoda get()
@@ -81,8 +121,8 @@ public class Brain extends Actor
     public static int n=10; // numarul de locuri in careu
     public static int mutari; //pt a putea indentifica egalitatea
     //daca mutari == 100 => egalitate
-    Map<Integer, Integer> mapNeigh;
-    int[][] offsetNeigh = {{-1,-1,-1,0,0,1,1,1},{-1,0,1,-1,1,-1,0,1}};
+    int[] mapNeigh;
+    final int[][] offsetNeigh = {{-1,-1,-1,0,0,1,1,1},{-1,0,1,-1,1,-1,0,1}};
     boolean ok=true;
     public Brain()
     {
@@ -95,18 +135,20 @@ public class Brain extends Actor
         else CurrentPlayer = Type.Y;
         createMap();
     }
+
     private void createMap()
     {
-        mapNeigh = new HashMap<Integer, Integer>();
-        mapNeigh.put(4,0);
-        mapNeigh.put(5,0);
-        mapNeigh.put(7,1);
-        mapNeigh.put(2,1);
-        mapNeigh.put(8,2);
-        mapNeigh.put(1,2);
-        mapNeigh.put(6,3);
-        mapNeigh.put(3,3);
+        mapNeigh = new int[8];
+        mapNeigh[0] = 2;
+        mapNeigh[1] = 1;
+        mapNeigh[2] = 3;
+        mapNeigh[3] = 0;
+        mapNeigh[4] = 0;
+        mapNeigh[5] = 3;
+        mapNeigh[6] = 1;
+        mapNeigh[7] = 2;
     }
+
     public void creare_grid()
     {
         MyList<Element> TempList;
@@ -123,11 +165,12 @@ public class Brain extends Actor
             Elements.add(TempList);
         }
     }
+
     protected Element clicked(Gride Clicked)
     {
         MyList<Element> desiredLine;
         Element desiredNeigh;
-        
+
         Element justClicked = Elements.get(Clicked.getCoordX()).get(Clicked.getCoordY());
         justClicked.setStatus(CurrentPlayer);
         // ne adaugam pe noi pe liniile componente
@@ -146,13 +189,15 @@ public class Brain extends Actor
                 if(desiredNeigh.getStatus() == justClicked.getStatus())
                 {
                     //trebuie sa stim in ce lista de linii il adaugam 
-                    justClicked.addOnLine(/,desiredNeigh);
-                    //mapeaza manual cu un vector bidimensional |
+                    justClicked.addLineOnLine(mapNeigh[i],desiredNeigh.getLine(mapNeigh[i]));
                 }
             }
         }
+        //dupa ce am gasit toti vecinii, sincronizam listele lor, cu lista din elementul curent 
+        justClicked.sincLines();
         return justClicked;
     }
+
     public void act() 
     {
         if(ok)
@@ -161,6 +206,7 @@ public class Brain extends Actor
             creare_grid();
         }
     } 
+
     public static Type getNextPlayer()
     {
         return CurrentPlayer;
