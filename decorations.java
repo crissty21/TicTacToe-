127b35ad4eb7bb614e91971afc8daf36de3fad31
CristianNicolae <cristiannicolae.cindea@ulbsibiu.ro>
2022-06-21 19:20:00 +0300

    init

---
+++ decorations.java
@@ -0,0 +1,17 @@
+import greenfoot.*;
+
+public class decorations extends Actor {
+    private boolean bFadeIn;
+    public decorations(GreenfootImage image) {
+        setImage(image);
+        bFadeIn = false;
+    }
+    public void fadeIn(int speed)
+    {
+
+    }
+    
+    public void act() {
+        
+    }
+}
